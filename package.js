d05b195e3b9528e0fd32c5e86f518be1e18d0b96
Josh <c13joshua.christman@gmail.com>
2015-01-08 14:33:39 -0500

    - Version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'jchristman:meteor-os',
   summary: 'Meteor package to easily allow desktop-like applications in meteor',
-  version: '1.0.2',
+  version: '1.0.3',
   git: 'https://github.com/jchristman/meteor-os'
 });
 
